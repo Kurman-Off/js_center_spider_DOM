26640a894bb518b50e26dd5db58cfa4110b8e07a
Your Name <ruslankurman13@gmail.com>
2024-07-24 17:45:50 +0300

    make build

---
+++ index.0a8265fa.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=e.getBoundingClientRect(),o=t.getBoundingClientRect(),c=(n.height-o.height)/2,d=(n.width-o.width)/2;t.style.top="".concat(c,"px"),t.style.left="".concat(d,"px")});
+//# sourceMappingURL=index.0a8265fa.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    const wall = document.querySelector(".wall");
+    const spider = document.querySelector(".spider");
+    const wallRect = wall.getBoundingClientRect();
+    const spiderRect = spider.getBoundingClientRect();
+    const topPosition = (wallRect.height - spiderRect.height) / 2;
+    const leftPosition = (wallRect.width - spiderRect.width) / 2;
+    spider.style.top = `${topPosition}px`;
+    spider.style.left = `${leftPosition}px`;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
